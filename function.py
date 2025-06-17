828671e3165c00ca735069ea5668f295e715dbd5
takizawaseid0u <takizawaseidou@tutamail.com>
2025-06-17 12:20:39 +0300

    2nd

--- function.py
+++ function.py
@@ -11,10 +11,10 @@ class funct:
         self.y = 0
 
     def ff(self):
-        self.y += sin(math.degrees(self.a))/cos(math.degrees(self.b)) + sqrt(self.c)
+        self.y += sin((self.a))/cos((self.b)) + sqrt(self.c)
         print (self.y)
-        print (sin(math.degrees(self.a)))
-        print (cos(math.degrees(self.b)))
+        print (sin(self.a))
+        print (cos(self.b))
     
 
 
@@ -23,3 +23,6 @@ funct.ff(fu)
 'первый тест на работу без ошибок'
 'ожидается ответ 8, полученный ответ 7.89'
 'в чем ошибка: не учел что питон считает в радианах'
+
+'второй тест с учетом углов в радианах'
+'ожидаемый ответ 7.893996, полученный ответ 7.893996'
